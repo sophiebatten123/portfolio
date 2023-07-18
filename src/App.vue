bf2e9096444432b3fc8ecf551b2b2346aa837055
Sophie Batten <sophie.batten@classroomsecrets.co.uk>
2023-07-18 19:53:23 +0100

    Initial styling including the about section and the languages section has been added to the site

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,10 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+  <div class="">
+    <div class="purple-bg w-full py-6">
+      <nav class="w-3/5 mx-auto text-white text-right text-2xl pr-12">
+        <router-link to="/">Home</router-link>
+      </nav>
+    </div>
     <router-view/>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,104 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="w-full">
+    <div class="w-3/5 mx-auto py-8 relative">
+      <div class="grid grid-cols-2">
+        <div>
+          <h1 class="text-5xl text-gray-600">&lt;>Hello!&lt;/></h1>
+          <h2 class="text-gray-600 text-3xl mt-8">About Me</h2>
+          <h2 class="orange-text text-2xl mt-2">Sophie Batten - Full Stack Developer</h2>
+          <p class="pr-6 pt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis mauris neque, sed faucibus massa finibus non. Sed id massa nibh. Aliquam id neque eu nunc commodo varius. Morbi pulvinar quis urna eu feugiat. Fusce quis consequat ante, et faucibus augue. Etiam ullamcorper aliquam imperdiet. Fusce quis consequat ante, et faucibus augue. Etiam ullamcorper aliquam imperdiet.</p>
+        </div>
+        <div class="absolute right-0 w-1/2">
+          <img src="../assets/me.webp" alt="An image of me">
+        </div>
+      </div>
+    </div>
+    <div class="purple-bg">
+      <div class="w-3/5 mx-auto py-6">
+        <button class="rounded-full text-white border-2 px-4 w-1/5 text-center py-2">Contact Me</button>
+        <button class="rounded-full text-white orange-bg border-2 px-4 w-1/5 text-center py-2 ml-2">Download CV</button>
+      </div>
+      <div class="grid grid-cols-7">
+        <img src="../assets/html.webp" alt="An image of the HTML logo" class="w-24 py-6 mx-auto justify-center">
+        <img src="../assets/css.webp" alt="An image of the CSS logo" class="w-24 py-6 mx-auto">
+        <img src="../assets/javascript.webp" alt="An image of the JavaScript logo" class="w-24 py-6 mx-auto">
+        <img src="../assets/laravel.webp" alt="An image of the JavaScript logo" class="w-24 py-6 mx-auto">
+        <img src="../assets/python.webp" alt="An image of the JavaScript logo" class="w-24 py-6 mx-auto">
+        <img src="../assets/vue.webp" alt="An image of the JavaScript logo" class="w-24 py-6 mx-auto">
+        <img src="../assets/django.webp" alt="An image of the JavaScript logo" class="w-24 py-6 mx-auto">
+      </div>
+      <div class="grid grid-cols-7 text-white text-center pb-4">
+        <div class="px-2">
+          <h3 class="text-xl">HTML</h3>
+          <p>Helvetica Light is an easy-to-read font, with tall and narrow letters, that works well on</p>
+        </div>
+        <div class="px-2">
+          <h3 class="text-xl">CSS</h3>
+          <p>Helvetica Light is an easy-to-read font, with tall and narrow letters, that works well on</p>
+        </div>
+        <div class="px-2">
+          <h3 class="text-xl">JavaScript</h3>
+          <p>Helvetica Light is an easy-to-read font, with tall and narrow letters, that works well on</p>
+        </div>
+        <div class="px-2">
+          <h3 class="text-xl">Laravel</h3>
+          <p>Helvetica Light is an easy-to-read font, with tall and narrow letters, that works well on</p>
+        </div>
+        <div class="px-2">
+          <h3 class="text-xl">Python</h3>
+          <p>Helvetica Light is an easy-to-read font, with tall and narrow letters, that works well on</p>
+        </div>
+        <div class="px-2">
+          <h3 class="text-xl">Vue</h3>
+          <p>Helvetica Light is an easy-to-read font, with tall and narrow letters, that works well on</p>
+        </div>
+        <div class="px-2">
+          <h3 class="text-xl">Django</h3>
+          <p>Helvetica Light is an easy-to-read font, with tall and narrow letters, that works well on</p>
+        </div>
+      </div>
+    </div>
+    <div class="w-3/5 mx-auto">
+      <h2 class="text-center text-3xl text-gray-600 mt-4">My Work Experience</h2>
+      <div class="grid grid-cols-2">
+        <div class="border-r-2 border-dotted border-gray-600 h-80 my-8 relative">
+          <div class="w-5 h-5 rounded-full orange-bg absolute right-0 -mt-2 -mr-2.5"></div>
+          <div class="w-5 h-5 rounded-full orange-bg absolute right-0 top-20 -mr-2.5"></div>
+          <div class="w-5 h-5 rounded-full orange-bg absolute right-0 top-40 -mr-2.5"></div>
+          <div class="w-5 h-5 rounded-full orange-bg absolute right-0 top-80 -mr-2.5"></div>
+          <p>hello</p>
+        </div>
+      </div>
+    </div>
+    <div class="purple-bg">
+      <div class="grid grid-cols-3">
+        <div>
+          <p>Project 1</p>
+        </div>
+        <div>
+          <p>Project 2</p>
+        </div>
+        <div>
+          <p>Project 3</p>
+        </div>
+        <div>
+          <p>Project 4</p>
+        </div>
+        <div>
+          <p>Project 5</p>
+        </div>
+        <div>
+          <p>Project 6</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
