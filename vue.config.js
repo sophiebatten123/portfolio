15c83c52f4999f89e6b90bec6824e226979096be
Sophie Batten <sophie.batten@classroomsecrets.co.uk>
2023-07-20 22:29:27 +0100

    added dist

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/', // Replace 'repository-name' with your actual repository name
+  publicPath: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
 });
