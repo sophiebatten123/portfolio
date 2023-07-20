43dfea922cbd2207bd31452946adf6e2b0a47703
Sophie Batten <sophie.batten@classroomsecrets.co.uk>
2023-07-20 22:06:44 +0100

    new dist folder for build has been added

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  publicPath: '/portfolio/', // Replace 'repository-name' with your actual repository name
+};
